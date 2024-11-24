e077bfd97ef58e2a9728934904b47fbdf8702bb0
Guten Ye <ywzhaifei@gmail.com>
2024-11-24 22:17:24 +0800

    remove export all

--- packages/child_process/childProcessUtils.ts
+++ packages/child_process/childProcessUtils.ts
@@ -1,7 +1,5 @@
 import { spawn } from 'node:child_process'
 
-export * from 'node:child_process'
-
 export function spawnPlus(command: SpawnArgs[0], options: SpawnArgs[2]) {
   let stderr = ''
   const newOptions = { shell: true, ...options }
