450d6eaaa290f2a3ed876664b47bda2d0c58eaf1
Guten Ye <ywzhaifei@gmail.com>
2024-11-24 22:14:28 +0800

    add child_process

---
+++ packages/child_process/__tests__/childProcessUtils.test.ts
@@ -0,0 +1,28 @@
+import { afterEach, describe, expect, it, jest, mock } from 'bun:test'
+import { EventEmitter } from 'node:events'
+import { spawnPlus } from '../childProcessUtils'
+
+mock.module('node:child_process', () => ({
+  spawn: mock(() => {
+    const emitter = new EventEmitter()
+    emitter.stderr = new EventEmitter()
+    emitter.stdout = new EventEmitter()
+    return emitter
+  }),
+}))
+
+afterEach(() => {
+  jest.restoreAllMocks()
+})
+
+describe('spawnPlus', () => {
+  it('errorPlus', () => {
+    const child = spawnPlus('not-found')
+    child.on('errorPlus', (error) => {
+      expect(error).toEqual({ exitCode: 1, stderr: 'stderr' })
+    })
+    child.stderr.emit('data', 'stderr')
+    child.emit('exit', 1)
+    expect.assertions(1)
+  })
+})
