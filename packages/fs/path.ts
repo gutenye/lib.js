6387075bc9fde561875d7b90d8f2ffbe78ff48b8
Guten Ye <ywzhaifei@gmail.com>
2024-12-26 21:55:05 +0800

    fix: remove export

--- packages/fs/path.ts
+++ packages/fs/path.ts
@@ -3,7 +3,7 @@ import fs from './fs'
 
 // suffix name
 // a.js -> a-suffix.js
-export function suffix(path: string, suffix: string) {
+function suffix(path: string, suffix: string) {
   const { dir, name, ext } = nodePath.parse(path)
   return nodePath.join(dir, `${name}${suffix}${ext}`)
 }
@@ -11,7 +11,7 @@ export function suffix(path: string, suffix: string) {
 /**
  * If path exists, return a new path with a number appended to it.
  */
-export async function genUniquePath(path: string): Promise<string> {
+async function genUniquePath(path: string): Promise<string> {
   const { dir, name, ext } = nodePath.parse(path)
   let newPath = path
   let index = 1
@@ -25,7 +25,7 @@ export async function genUniquePath(path: string): Promise<string> {
 /*
  * Check path has ext
  */
-export function hasExt(path: string, exts: string[]) {
+function hasExt(path: string, exts: string[]) {
   const ext = nodePath.extname(path).slice(1)
   return exts.includes(ext)
 }
